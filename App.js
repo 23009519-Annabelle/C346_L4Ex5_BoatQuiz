61bef7769e60ceba69d458e230ad09ef6640e3bf
23009519-Annabelle <23009519@myrp.edu.sg>
2024-10-23 15:17:54 +0800

    C346_L2Boats

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StatusBar, ScrollView, Text} from 'react-native';
+import Boat from "./components/Boat.js"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+    return (
+        <ScrollView>
+            <StatusBar hidden={true} />
+            <Text style={{color:'#5A5A5A', marginBottom: 20}}>GetABoat - For Sale</Text>
+            <Boat title="Sea Ray 500 Sundancer" icon_name="sailboat" image={require("./img/sea_ray.jpg")}  desc="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away."/>
+            <Boat title="Four Winns Horizon 180" icon_name="sailboat" image={require("./img/four_winns.jpg")}  desc="A sporty look and refined details truly set the Horizon 180 above all others."/>
+            <Boat title="Flipper 640 ST" icon_name="sailboat" image={require("./img/flipper.jpg")} desc="A modern take on the classic, traditional hardtop and perfect for a family picnic." />
+            <Boat title="Princess V48" icon_name="sailboat" image={require("./img/princess.jpg")} desc="There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior." />
+            <Boat title="Bayliner 175 Bowrider" icon_name="sailboat" image={require("./img/bayliner.jpg")} desc="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance" />
+            <Boat title="Fairline Targa 47" icon_name="sailboat" image={require("./img/fairline.jpg")} desc="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit." />
+        </ScrollView>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ components/Boat.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text ,Image} from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome6";
+
+const Boat = ({title, icon_name, image, desc}) => {
+    return (
+        <View>
+            <Icon name={icon_name} size={20} color="#5A5A5A">  <Text>{title}</Text></Icon>
+            <Text style={{color:'#5A5A5A'}}>{desc}</Text>
+            <Image source= {image}
+                   style={{width:450, height: 300, marginBottom: 50}} />
+        </View>
+    );
+}
+export default Boat;
