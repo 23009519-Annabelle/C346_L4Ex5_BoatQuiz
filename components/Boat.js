1aa758bebc2843d99db02d34e0573e86be506eda
23009519-Annabelle <23009519@myrp.edu.sg>
2024-10-23 15:19:04 +0800

    C346_L2Boats

--- components/Boat.js
+++ components/Boat.js
@@ -8,7 +8,7 @@ const Boat = ({title, icon_name, image, desc}) => {
             <Icon name={icon_name} size={20} color="#5A5A5A">  <Text>{title}</Text></Icon>
             <Text style={{color:'#5A5A5A'}}>{desc}</Text>
             <Image source= {image}
-                   style={{width:450, height: 300, marginBottom: 50}} />
+                   style={{width:450, height: 300, marginBottom: 20}} />
         </View>
     );
 }
