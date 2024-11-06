b18f31f6b697ef2a8161f2c3eee267bc0da1a0c2
23009519-Annabelle <23009519@myrp.edu.sg>
2024-11-06 21:01:07 +0800

    C346_L4Ex5Boats

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import {StatusBar, ScrollView, Text} from 'react-native';
+import { StatusBar, ScrollView, Text, StyleSheet } from 'react-native';
 import Boat from "./components/Boat.js"
 
+const styles = StyleSheet.create({
+    container: {
+    },
+    title: {
+        color: 'grey',
+        marginBottom: 20,
+        textAlign: 'center',
+        fontSize: 20,
+    },
+    boatBox: {
+        marginTop: 30,
+        borderWidth: 1,
+        borderColor: 'black',
+    },
+});
 const App = () => {
     return (
-        <ScrollView>
+        <ScrollView style={styles.boatBox}>
             <StatusBar hidden={true} />
-            <Text style={{color:'#5A5A5A', marginBottom: 20}}>GetABoat - For Sale</Text>
-            <Boat title="Sea Ray 500 Sundancer" icon_name="sailboat" image={require("./img/sea_ray.jpg")}  desc="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away."/>
-            <Boat title="Four Winns Horizon 180" icon_name="sailboat" image={require("./img/four_winns.jpg")}  desc="A sporty look and refined details truly set the Horizon 180 above all others."/>
+            <Text style={styles.title}>GetABoat - For Sale</Text>
+            <Boat  title="Sea Ray 500 Sundancer" icon_name="sailboat" image={require("./img/sea_ray.jpg")} desc="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away." />
+            <Boat title="Four Winns Horizon 180" icon_name="sailboat" image={require("./img/four_winns.jpg")} desc="A sporty look and refined details truly set the Horizon 180 above all others." />
             <Boat title="Flipper 640 ST" icon_name="sailboat" image={require("./img/flipper.jpg")} desc="A modern take on the classic, traditional hardtop and perfect for a family picnic." />
             <Boat title="Princess V48" icon_name="sailboat" image={require("./img/princess.jpg")} desc="There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior." />
-            <Boat title="Bayliner 175 Bowrider" icon_name="sailboat" image={require("./img/bayliner.jpg")} desc="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance" />
+            <Boat title="Bayliner 175 Bowrider" icon_name="sailboat" image={require("./img/bayliner.jpg")} desc="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance." />
             <Boat title="Fairline Targa 47" icon_name="sailboat" image={require("./img/fairline.jpg")} desc="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit." />
         </ScrollView>
     );
--- components/Boat.js
+++ components/Boat.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import { View, Text ,Image} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import Icon from "react-native-vector-icons/FontAwesome6";
 
-const Boat = ({title, icon_name, image, desc}) => {
+const Boat = ({ title, icon_name, image, desc, style }) => {
     return (
-        <View>
-            <Icon name={icon_name} size={20} color="#5A5A5A">  <Text>{title}</Text></Icon>
-            <Text style={{color:'#5A5A5A'}}>{desc}</Text>
-            <Image source= {image}
-                   style={{width:450, height: 300, marginBottom: 20}} />
+        <View style={[styles.container, style]}>
+            <Icon name={icon_name} size={30} color="black" />
+            <Image source={image} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.desc}>{desc}</Text>
         </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'grey',
+        marginBottom: 20,
+    },
+    image: {
+        width: '100%',
+        height: 200,
+        resizeMode: 'cover',
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginVertical: 5,
+        color: 'black',
+    },
+    desc: {
+        fontSize: 14,
+        color: 'grey',
+    },
+});
+
 export default Boat;
